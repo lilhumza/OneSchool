6fe683b6297981a6979b14e1cae43a3a3b413d61
Draftyshark <48521743+Draftyshark@users.noreply.github.com>
2019-12-08 15:41:57 -0500

    Update mainWindow.java

--- src/mainWindow.java
+++ src/mainWindow.java
@@ -20,15 +20,21 @@ import javax.swing.table.TableModel;
 import java.awt.SystemColor;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Arrays;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
-public class mainWindow implements TableModelListener{
+public class mainWindow implements TableModelListener, ItemListener{
 
 	private JFrame frame;
-	JScrollPane scrollPane = new JScrollPane();
+	JScrollPane SPMarkbook = new JScrollPane();
+	JComboBox comboBox = new JComboBox();
+	JLabel lblNewLabel_1 = new JLabel("Markbook");
 
 	/**
 	 * Launch the application.
@@ -67,71 +73,80 @@ public class mainWindow implements TableModelListener{
 		
 		ImageIcon image = new ImageIcon("C:/Users/draft/eclipse-workspace/OneSchool/src/kisspng-dashboard-business-intelligence-management-informa-dashboard-5adceebbd6e940.5844354015244284758803.jpg");
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
+		JPanel classXMarkbook = new JPanel();
+		classXMarkbook.setBackground(Color.WHITE);
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 272, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 2138, GroupLayout.PREFERRED_SIZE))
+					.addComponent(classXMarkbook, GroupLayout.PREFERRED_SIZE, 2138, GroupLayout.PREFERRED_SIZE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addComponent(panel, GroupLayout.DEFAULT_SIZE, 949, Short.MAX_VALUE)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 938, GroupLayout.PREFERRED_SIZE)
+					.addComponent(classXMarkbook, GroupLayout.PREFERRED_SIZE, 938, GroupLayout.PREFERRED_SIZE)
 					.addGap(11))
 		);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(Color.LIGHT_GRAY);
 
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(scrollPane, Alignment.LEADING)
-						.addComponent(panel_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addContainerGap(26, Short.MAX_VALUE))
+		GroupLayout gl_classXMarkbook = new GroupLayout(classXMarkbook);
+		gl_classXMarkbook.setHorizontalGroup(
+			gl_classXMarkbook.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_classXMarkbook.createSequentialGroup()
+					.addGroup(gl_classXMarkbook.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(panel_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(SPMarkbook, Alignment.LEADING))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_panel_1.createSequentialGroup()
+		gl_classXMarkbook.setVerticalGroup(
+			gl_classXMarkbook.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_classXMarkbook.createSequentialGroup()
 					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 796, Short.MAX_VALUE)
+					.addComponent(SPMarkbook, GroupLayout.DEFAULT_SIZE, 796, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		
-		JLabel lblNewLabel_1 = new JLabel("Markbook");
+
 		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 40));
+		
+		comboBox.setModel(new DefaultComboBoxModel(marksScreen.calsses));
+		SimpleTableDemo1(comboBox);
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
 		gl_panel_2.setHorizontalGroup(
-			gl_panel_2.createParallelGroup(Alignment.LEADING)
+			gl_panel_2.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblNewLabel_1)
-					.addContainerGap(1909, Short.MAX_VALUE))
+					.addContainerGap(1939, Short.MAX_VALUE))
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap(2039, Short.MAX_VALUE)
+					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(32))
 		);
 		gl_panel_2.setVerticalGroup(
 			gl_panel_2.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_2.createSequentialGroup()
 					.addGap(34)
 					.addComponent(lblNewLabel_1)
-					.addContainerGap(44, Short.MAX_VALUE))
+					.addGap(18)
+					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		panel_2.setLayout(gl_panel_2);
-		marksScreen.displayTable(scrollPane);
-		panel_1.setLayout(gl_panel_1);
+		marksScreen.displayTable(SPMarkbook);
+		classXMarkbook.setLayout(gl_classXMarkbook);
 		
 		
 		JButton btnDashboard = new JButton("Dashboard");
 		btnDashboard.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				scrollPane.setVisible(false);
+				classXMarkbook.setVisible(false);
 				
 			}
 		});
@@ -145,7 +160,7 @@ public class mainWindow implements TableModelListener{
 		JButton btnMarkbook = new JButton("Markbook");
 		btnMarkbook.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				scrollPane.setVisible(true);
+				classXMarkbook.setVisible(true);
 			}
 		});
 		btnMarkbook.setForeground(Color.WHITE);
@@ -157,7 +172,7 @@ public class mainWindow implements TableModelListener{
 		JButton btnInventory = new JButton("Inventory");
 		btnInventory.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				scrollPane.setVisible(false);
+				classXMarkbook.setVisible(false);
 			}
 		});
 		btnInventory.setForeground(Color.WHITE);
@@ -169,7 +184,7 @@ public class mainWindow implements TableModelListener{
 		JButton btnStats = new JButton("Stats");
 		btnStats.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				scrollPane.setVisible(false);
+				classXMarkbook.setVisible(false);
 			}
 		});
 		btnStats.setForeground(Color.WHITE);
@@ -219,7 +234,7 @@ public class mainWindow implements TableModelListener{
 					.addContainerGap(415, Short.MAX_VALUE))
 		);
 		marksScreen.calculateMark(marksScreen.marks);
-	    marksScreen.createTable(marksScreen.scrollpane);
+	    marksScreen.createTable(marksScreen.SPMarkbook, marksScreen.marks, marksScreen.assignments);
 	    SimpleTableDemo(marksScreen.table);
 		panel.setLayout(gl_panel);
 		frame.getContentPane().setLayout(groupLayout);
@@ -227,6 +242,10 @@ public class mainWindow implements TableModelListener{
 	
 	public void SimpleTableDemo(JTable table) {
         table.getModel().addTableModelListener(this);
+    }
+	
+	public void SimpleTableDemo1(JComboBox table) {
+		comboBox.addItemListener(this);
     }
 
 	public void tableChanged(TableModelEvent e) {
@@ -236,14 +255,18 @@ public class mainWindow implements TableModelListener{
 	     String columnName = model.getColumnName(column);
 	     Object data = model.getValueAt(row, column);
 
-	     System.out.println(columnName);
-	     System.out.println(row);
-	     System.out.println(column);
 	     Arrays.deepToString(marksScreen.marks);
 	     marksScreen.marks[row][column] = (String) data;
 	     marksScreen.calculateMark(marksScreen.marks);
-	     marksScreen.createTable(marksScreen.scrollpane);
+	     marksScreen.createTable(marksScreen.SPMarkbook, marksScreen.marks, marksScreen.assignments);
 	     SimpleTableDemo(marksScreen.table);
 		
 	}
+	public void itemStateChanged(ItemEvent e) 
+    { 
+        if (e.getSource() == comboBox) { 
+  
+        	lblNewLabel_1.setText((String) comboBox.getSelectedItem()); 
+        } 
+    } 
 }
