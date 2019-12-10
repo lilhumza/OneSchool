4e2826a5cb0e4a24da457812c9d10cea4fa4a491
Ymet <55714286+Ymet@users.noreply.github.com>
2019-12-10 12:58:08 -0500

    Create Login Screen.java

---
+++ src/Login Screen.java
@@ -0,0 +1,71 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.JEditorPane;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class LoginScreen {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginScreen window = new LoginScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1000, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(298, 129, 294, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(298, 180, 294, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblUserName = new JLabel("User Name:");
+		lblUserName.setBounds(214, 132, 74, 14);
+		frame.getContentPane().add(lblUserName);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setBounds(221, 183, 56, 14);
+		frame.getContentPane().add(lblPassword);
+		
+		JLabel lblLogin = new JLabel("Login");
+		lblLogin.setBounds(406, 58, 46, 14);
+		frame.getContentPane().add(lblLogin);
+	}
+}
