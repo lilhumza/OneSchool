5c32880858747890fcdabf21b27d93c056212ac2
DraftyShark <48521743+DraftyShark@users.noreply.github.com>
2019-12-17 14:40:05 -0500

    Update dashBoard.java

--- src (new)/dashBoard.java
+++ src (new)/dashBoard.java
@@ -1,18 +1,13 @@
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Component;
 import java.awt.ComponentOrientation;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
+import javax.swing.border.Border;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
@@ -20,24 +15,12 @@ import java.awt.event.MouseEvent;
 
 public class dashBoard {
 
-	private JFrame dashboardFrame;
+	JFrame dashboardFrame;
 	private JLabel lblWelcome;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					dashBoard window = new dashBoard();
-					window.dashboardFrame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
 	/**
 	 * Create the application.
@@ -50,21 +33,18 @@ public class dashBoard {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		//Create JFrame
 		dashboardFrame = new JFrame();
 		dashboardFrame.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		dashboardFrame.setBounds(100, 100, 1000, 500);
-		dashboardFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		dashboardFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		dashboardFrame.getContentPane().setLayout(null);
 		
-		//Create and design the NavBar area
 		JPanel navPanel = new JPanel();
 		navPanel.setBounds(0, 0, 160, 455);
 		navPanel.setBackground(Color.DARK_GRAY);
 		dashboardFrame.getContentPane().add(navPanel);
 		navPanel.setLayout(null);
 		
-		//Menu label for NavBar
 		JLabel lblMenu = new JLabel("Menu");
 		lblMenu.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMenu.setBounds(28, 3, 106, 57);
@@ -72,146 +52,176 @@ public class dashBoard {
 		lblMenu.setForeground(Color.WHITE);
 		navPanel.add(lblMenu);
 		
-		//MarkBook Button on NavBar
 		JButton btnMarkBook = new JButton("Mark Book");
 		btnMarkBook.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							markbook markbookw = new markbook();
+							markbookw.frame.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
 			}
 		});
-		btnMarkBook.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnMarkBook.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnMarkBook.setBounds(19, 156, 115, 29);
 		navPanel.add(btnMarkBook);
+		btnMarkBook.setForeground(Color.WHITE);
+		btnMarkBook.setBackground(Color.DARK_GRAY);
+		Border emptyBorder = BorderFactory.createEmptyBorder();
+		btnMarkBook.setBorder(emptyBorder);
+		btnMarkBook.setFocusPainted(false);
+
 		
-		//GymInventory Button on NavBar
 		JButton btnGymInventory = new JButton("GYM Inventory");
+		btnGymInventory.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							Inventory InventoryWindow = new Inventory();
+							InventoryWindow.frame.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
 		btnGymInventory.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		btnGymInventory.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnGymInventory.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnGymInventory.setBounds(18, 237, 115, 29);
 		navPanel.add(btnGymInventory);
+		btnGymInventory.setForeground(Color.WHITE);
+		btnGymInventory.setBackground(Color.DARK_GRAY);
+		btnGymInventory.setBorder(emptyBorder);
+		btnGymInventory.setFocusPainted(false);
 		
-		//SncInventory Button on NavBar
 		JButton btnScienceInventory = new JButton("SNC Inventory");
 		btnScienceInventory.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		btnScienceInventory.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnScienceInventory.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnScienceInventory.setBounds(19, 321, 115, 29);
 		navPanel.add(btnScienceInventory);
+		btnScienceInventory.setForeground(Color.WHITE);
+		btnScienceInventory.setBackground(Color.DARK_GRAY);
+		btnScienceInventory.setBorder(emptyBorder);
+		btnScienceInventory.setFocusPainted(false);
 		
-		//Attendance button on NavBar
 		JButton btnAttendance = new JButton("Attendance");
 		btnAttendance.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		btnAttendance.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnAttendance.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnAttendance.setBounds(19, 76, 115, 29);
 		navPanel.add(btnAttendance);
+		btnAttendance.setForeground(Color.WHITE);
+		btnAttendance.setBackground(Color.DARK_GRAY);
+		btnAttendance.setBorder(emptyBorder);
+		btnAttendance.setFocusPainted(false);
 		
-		//Log Out Button on NavBar
 		JButton btnLogOut = new JButton("Log Out");
 		btnLogOut.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 			}
 		});
-		btnLogOut.setFont(new Font("Times New Roman", Font.BOLD, 12));
+		btnLogOut.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnLogOut.setBounds(19, 392, 115, 29);
 		navPanel.add(btnLogOut);
+		btnLogOut.setForeground(Color.WHITE);
+		btnLogOut.setBackground(Color.DARK_GRAY);
+		btnLogOut.setBorder(emptyBorder);
+		btnLogOut.setFocusPainted(false);
 		
-		//Quick Stats - Attendance Metric
 		JPanel attendancePanel = new JPanel();
 		attendancePanel.setBounds(192, 250, 165, 165);
 		attendancePanel.setBackground(Color.LIGHT_GRAY);
 		dashboardFrame.getContentPane().add(attendancePanel);
 		attendancePanel.setLayout(null);
 		
-		//Attendance Metric Label
-		JLabel lblAttendance = new JLabel("16/29");
-		lblAttendance.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAttendance.setBounds(0, 0, 165, 165);
-		lblAttendance.setFont(new Font("Times New Roman", Font.PLAIN, 40));
-		attendancePanel.add(lblAttendance);
+		JLabel label = new JLabel("16/29");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setBounds(0, 0, 165, 165);
+		label.setFont(new Font("Times New Roman", Font.PLAIN, 40));
+		attendancePanel.add(label);
 		
-		//Present Label
 		JLabel lblPresent = new JLabel("Present");
 		lblPresent.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		lblPresent.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPresent.setBounds(25, 129, 125, 20);
 		attendancePanel.add(lblPresent);
 		
-		//Quick Stats - Gym Inventory
 		JPanel gymPanel = new JPanel();
 		gymPanel.setBounds(390, 250, 165, 165);
 		gymPanel.setBackground(Color.GRAY);
 		dashboardFrame.getContentPane().add(gymPanel);
 		gymPanel.setLayout(null);
 		
-		//Gym Equipment Metric Label
-		JLabel lblGymLabel = new JLabel("17/32");
-		lblGymLabel.setBounds(0, 0, 165, 165);
-		lblGymLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblGymLabel.setFont(new Font("Times New Roman", Font.PLAIN, 40));
-		gymPanel.add(lblGymLabel);
+		JLabel lblNewLabel = new JLabel("17/32");
+		lblNewLabel.setBounds(0, 0, 165, 165);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 40));
+		gymPanel.add(lblNewLabel);
 		
-		//Gym Equipment Label
 		JLabel lblGymEquip = new JLabel("GYM Equipment");
 		lblGymEquip.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		lblGymEquip.setBounds(15, 129, 135, 20);
 		gymPanel.add(lblGymEquip);
 		
-		//Quick Stats - SNC Inventory
 		JPanel sncPanel = new JPanel();
 		sncPanel.setBounds(582, 250, 165, 165);
 		sncPanel.setBackground(Color.DARK_GRAY);
 		dashboardFrame.getContentPane().add(sncPanel);
 		sncPanel.setLayout(null);
 		
-		//SNC Equipment Metric Label
-		JLabel lblSncLabel = new JLabel("ZERO");
-		lblSncLabel.setBounds(0, 0, 165, 165);
-		lblSncLabel.setFont(new Font("Times New Roman", Font.PLAIN, 40));
-		lblSncLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		sncPanel.add(lblSncLabel);
+		JLabel lblNewLabel_1 = new JLabel("ZERO");
+		lblNewLabel_1.setBounds(0, 0, 165, 165);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 40));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		sncPanel.add(lblNewLabel_1);
 		
-		//SNC Equipment Label
 		JLabel lblSncEquipment = new JLabel("SNC Equipment");
 		lblSncEquipment.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSncEquipment.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 		lblSncEquipment.setBounds(15, 129, 135, 20);
 		sncPanel.add(lblSncEquipment);
 		
-		//Quick Stats - Date & Time
 		JPanel datePanel = new JPanel();
 		datePanel.setBounds(776, 250, 165, 165);
 		datePanel.setBackground(Color.BLACK);
 		dashboardFrame.getContentPane().add(datePanel);
 		datePanel.setLayout(null);
 		
-		//Date Label
-		JLabel lblDate = new JLabel("10 DEC");
-		lblDate.setBounds(0, 0, 165, 165);
-		lblDate.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDate.setFont(new Font("Times New Roman", Font.PLAIN, 40));
-		lblDate.setForeground(Color.WHITE);
-		datePanel.add(lblDate);
+		JLabel lblDec = new JLabel("10 DEC");
+		lblDec.setBounds(0, 0, 165, 165);
+		lblDec.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDec.setFont(new Font("Times New Roman", Font.PLAIN, 40));
+		lblDec.setForeground(Color.WHITE);
+		datePanel.add(lblDec);
 		
-		//Time Label
-		JLabel lblTime = new JLabel("1:42 PM");
-		lblTime.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		lblTime.setForeground(Color.WHITE);
-		lblTime.setBounds(51, 129, 69, 20);
-		datePanel.add(lblTime);
+		JLabel lblPm = new JLabel("1:42 PM");
+		lblPm.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblPm.setForeground(Color.WHITE);
+		lblPm.setBounds(51, 129, 69, 20);
+		datePanel.add(lblPm);
+		//txtPrivateNotes.setColumns(10);
 		
-		//Welcome Label
 		lblWelcome = new JLabel("Welcome to OneSchool");
 		lblWelcome.setBounds(192, 45, 300, 45);
 		lblWelcome.setFont(new Font("Tahoma", Font.BOLD, 22));
@@ -219,7 +229,6 @@ public class dashBoard {
 		lblWelcome.setBackground(Color.DARK_GRAY);
 		dashboardFrame.getContentPane().add(lblWelcome);
 		
-		//Welcome Section TextArea
 		JTextArea txtrWelcome = new JTextArea();
 		txtrWelcome.setBackground(Color.WHITE);
 		txtrWelcome.setFont(new Font("Times New Roman", Font.PLAIN, 18));
@@ -229,7 +238,6 @@ public class dashBoard {
 		txtrWelcome.setBounds(192, 94, 363, 140);
 		dashboardFrame.getContentPane().add(txtrWelcome);
 		
-		//Private Notes TextArea
 		JTextArea txtrPrivateNotes = new JTextArea();
 		txtrPrivateNotes.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		txtrPrivateNotes.setLineWrap(true);
