a86cb4ece69840fe7691557acbf064f1986d80eb
DraftyShark <48521743+DraftyShark@users.noreply.github.com>
2020-01-10 13:34:32 -0500

    Update markPrediction.java

--- src (new)/markPrediction.java
+++ src (new)/markPrediction.java
@@ -1,80 +1,101 @@
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.awt.Font;
 
-import static java.util.Arrays.asList;
+import javax.swing.JTable;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
-public class markPrediction {
-    private static final List<Integer> x = asList(1, 2, 3, 4, 5, 6, 7);
-    private static final List<Integer> y = asList(2, 2, 3, 4, 5, 6, 7);
 
+public class marksTable {
 
-    static Integer numberOfDataValues = x.size();
+	static JTable table;
+	static JScrollPane SPMarkbook;
+	//Sample layout for the marks 2d array, which includes student names + their marks
+	static String[][] marks = {																 
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},				
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},		
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			{"A", "70", "50", "50", "50", "50", "0", "0", "0", "0", "0", "0"},
+			
+	};
+	//Sample layout for the headers, which includes user inputed assignments
+	public static String[] assignments = {
+		" ", "Assignment1", "Assignment1","Assignment1", "Assignment1", "Assignment1", "Calculated Marks"
+	};	
+	//Sample layout for the combobox, which includes user inputed classes
+	public static String[] calsses = {																		
+			"ICS4U0", "SCH4U0", "MHF4U0"																	
+	};
+	//Method to add all data to the table
+	public static void displayTable(JScrollPane SP) {
+		SPMarkbook = SP;
+		createTable(SP, marks, assignments);
+		
+	}
+	/* Calculate marks method takes in the String array marks
+	 * goes through each individual student marks and calculates
+	 * the marks based on the number of assignments.
+	 */
+	public static void calculateMark(String[][] marks) {
+		int total = 0;
+		for(int i = 0; i < marks.length; i++) {
+			for(int j = 1; j < assignments.length -1; j++) {
+				total += Integer.parseInt(marks[i][j]);
+			}
+			marks[i][assignments.length -1] = Integer.toString(total/(assignments.length - 2));
+			total = 0;
+		}
+	}
 
-    static List<Double> xSquared = x
-            .stream()
-            .map(position -> Math.pow(position, 2))
-            .collect(Collectors.toList());
+	
+	/* This method creates a table using the data from coloums (Student Names + Marks) and rows (Headers)
+	 * Also sets fonts for text inside the table
+	 * Finally, adds the table to the given scrollPane parameter
+	 */
+	public static void createTable(JScrollPane scrollPane, String[][] coloums, String[] rows) {
+		table = new JTable();
+		table.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		
+		calculateMark(marks);
+		table.setModel(new DefaultTableModel(
+				coloums, rows
+		));
 
-    static List<Double> ySquared = y
-            .stream()
-            .map(position -> Math.pow(position, 2))
-            .collect(Collectors.toList());
-
-    static List<Integer> xMultipliedByY = IntStream.range(0, numberOfDataValues)
-            .map(i -> x.get(i) * y.get(i))
-            .boxed()
-            .collect(Collectors.toList());
-
-    static Integer xSummed = x
-            .stream()
-            .reduce((prev, next) -> prev + next)
-            .get();
-
-    static Integer ySummed = y
-            .stream()
-            .reduce((prev, next) -> prev + next)
-            .get();
-
-    static Double sumOfXSquared = xSquared
-            .stream()
-            .reduce((prev, next) -> prev + next)
-            .get();
-
-    static Double sumOfYSquared = ySquared
-            .stream()
-            .reduce((prev, next) -> prev + next)
-            .get();
-
-    static Integer sumOfXMultipliedByY = xMultipliedByY
-            .stream()
-            .reduce((prev, next) -> prev + next)
-            .get();
-
-    private static Double predictForValue(int predictForDependentVariable) {
-
-        int slopeNominator = numberOfDataValues * sumOfXMultipliedByY - ySummed * xSummed;
-        Double slopeDenominator = numberOfDataValues * sumOfXSquared - Math.pow(xSummed, 2);
-        Double slope = slopeNominator / slopeDenominator;
-
-        double interceptNominator = ySummed - slope * xSummed;
-        double interceptDenominator = numberOfDataValues;
-        Double intercept = interceptNominator / interceptDenominator;
-
-        return (slope * predictForDependentVariable) + intercept;
-    }
-
-    static float correlationCoefficient()
-    {
-        float corr = (float)(numberOfDataValues * sumOfXMultipliedByY - xSummed * ySummed)/
-                (float)(Math.sqrt((numberOfDataValues * sumOfXSquared -
-                        xSummed * xSummed) * (numberOfDataValues * sumOfYSquared -
-                        ySummed * ySummed)));
-
-        return (float) (Math.pow(corr, 2) * 100);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(predictForValue(8) + " Predicted \n" + correlationCoefficient() + " Accuracy");
-    }
+		
+		table.getColumnModel().getColumn(1).setMinWidth(32);
+		Font f = new Font("Arial", Font.BOLD, 15);
+	    JTableHeader header = table.getTableHeader();
+	    header.setFont(f);
+		for(int i = 0; i < table.getRowCount(); i++) {
+			table.setRowHeight(i, 20);
+		}
+		for(int i = 0; i < table.getColumnCount(); i++) {
+			table.getColumnModel().getColumn(i).setPreferredWidth(125);
+		}
+		
+		
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		scrollPane.setViewportView(table);
+		
+	}
 }
