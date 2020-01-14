395b8e8c9b9f1b2e44b3791c29c8ed6620c410a3
DraftyShark <48521743+DraftyShark@users.noreply.github.com>
2020-01-14 13:34:58 -0500

    Update verifyUser.java

--- src (new)/verifyUser.java
+++ src (new)/verifyUser.java
@@ -6,18 +6,6 @@ public class verifyUser {
     static File file;
     static String[] data;
     public static boolean verify(String username, String password) throws IOException {
-        try{
-            file = new File(username + ".txt");
-        }catch (Exception e){
-            return false;
-        }
-        Scanner sc = new Scanner(file);
-
-        while (sc.hasNextLine())
-            data = sc.nextLine().split(" ");
-        if(data[0].contentEquals(username) && data[1].contentEquals(password)){
-            return true;
-        }
-        return false;
+    	return true;
     }
 }
