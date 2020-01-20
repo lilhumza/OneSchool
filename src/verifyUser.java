5994244056418db2ef588dd8e7933e338816dd4c
Ymet <55714286+Ymet@users.noreply.github.com>
2020-01-20 13:34:28 -0500

    Update verifyUser.java

--- src/verifyUser.java
+++ src/verifyUser.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+//Checks to see if the user name and password match the account info if its correct you will log in if not and error will pop up
 public class verifyUser {
     static File file;
     static String[] data;
